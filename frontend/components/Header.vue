506fa23745c21a934db87c652d86c4bcd048413e
Francesco Scheffczyk <francesco.scheffczyk@gmail.com>
2024-05-19 12:31:28 +0200

    feat: Create layout and page in Nuxt

---
+++ frontend/components/Header.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+	<header class="w-lvw h-auto py-2 flex justify-center items-center">
+		<h1>The Loophole Scorebook</h1>
+	</header>
+</template>
+
+<style scoped></style>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+	<div>
+		<p>Upload file...</p>
+	</div>
+</template>
+
+<style scoped></style>
