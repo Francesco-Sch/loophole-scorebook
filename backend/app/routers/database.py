6159894fd63b373c494c2070750357c8cfee0201
Francesco Scheffczyk <francesco.scheffczyk@gmail.com>
2024-05-28 00:08:55 +0200

    feat: Embed files into database

--- backend/app/routers/database.py
+++ backend/app/routers/database.py
@@ -1,4 +1,10 @@
+import os
+
+from app.utils.embeddings_function import embeddings_function
 from fastapi import APIRouter, HTTPException, UploadFile
+from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_qdrant import Qdrant
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 router = APIRouter(
     prefix="/database",
@@ -10,9 +16,53 @@ router = APIRouter(
 @router.post("/post", status_code=201, summary="Upload files to Vector Database")
 async def post(file: UploadFile):
     """
-    Upload files to Vector Database
+    Upload file to Vector Database
     """
     try:
-        return {"file": file}
+        # Save the file to the server
+        folder_path = "/media/uploads"
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+        with open(f"{folder_path}/{file.filename}", "wb") as buffer:
+            buffer.write(file.file.read())
+
+        ###########################
+        # LOADING
+        ###########################
+        file = PyMuPDFLoader(f"{folder_path}/{file.filename}")
+
+        file_as_document = file.load()
+
+        ###########################
+        # CHUNKING
+        ###########################
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=200
+        )
+        chunks = text_splitter.split_documents(file_as_document)
+
+        print(f"Number of chunks: {len(chunks)}")
+
+        ###########################
+        # EMBEDDING
+        ###########################
+        qdrant_collection = os.environ.get("QDRANT_COLLECTION")
+
+        print(f"Uploading to Qdrant collection: {qdrant_collection}")
+
+        Qdrant.from_documents(
+            url=os.environ.get("QDRANT_URL"),
+            prefer_grpc=False,
+            collection_name=qdrant_collection,
+            documents=chunks,
+            embedding=embeddings_function,
+        )
+
+        return {
+            "success": True,
+            "message": "File embedded successfully",
+            "documents": chunks,
+        }
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
--- frontend/server/api/embed/pdf.ts
+++ frontend/server/api/embed/pdf.ts
@@ -1,41 +1,53 @@
 import fs from "node:fs";
 
 export default defineEventHandler(async (event) => {
-	// Read the request body
-	const body = await readBody(event);
+	try {
+		// Read the request body
+		const body = await readBody(event);
 
-	// Get the list of files from the specified directory
-	const files = fs.readdirSync("/tmp/files");
+		// Get the list of files from the specified directory
+		const files = fs.readdirSync("/tmp/files");
 
-	// Find the file that matches the newFilename provided in the body
-	const file = files.find((f) => f === body.file.newFilename);
+		// Find the file that matches the newFilename provided in the body
+		const file = files.find((f) => f === body.file.newFilename);
 
-	// If the file is not found, return an error response
-	if (!file) {
-		return { success: false, message: "File not found" };
+		// If the file is not found, return an error response
+		if (!file) {
+			return { success: false, message: "File not found" };
+		}
+
+		// Construct the file path
+		const filePath = `/tmp/files/${file}`;
+
+		const fileData = fs.readFileSync(filePath);
+		const blob = new Blob([fileData], { type: "application/pdf" });
+
+		// Create a new FormData instance
+		const formData = new FormData();
+
+		// Append the file stream to the FormData instance
+		formData.append("file", blob, body.file.oldFilename);
+
+		// Perform the fetch request to the backend
+		const response = await $fetch("http://backend:8080/database/post", {
+			method: "POST",
+			body: formData,
+		});
+
+		// Log the response from the backend
+		console.log(response);
+
+		// Return a success response
+		return {
+			success: true,
+			message: "File embedded successfully",
+			response: response,
+		};
+	} catch (error) {
+		// Log any errors that occur
+		console.error(error);
+
+		// Return an error response
+		return { success: false, message: error };
 	}
-
-	// Construct the file path
-	const filePath = `/tmp/files/${file}`;
-
-	const fileData = fs.readFileSync(filePath);
-	const blob = new Blob([fileData], { type: "application/pdf" });
-
-	// Create a new FormData instance
-	const formData = new FormData();
-
-	// Append the file stream to the FormData instance
-	formData.append("file", blob);
-
-	// Perform the fetch request to the backend
-	const response = await $fetch("http://backend:8080/database/post", {
-		method: "POST",
-		body: formData,
-	});
-
-	// Log the response from the backend
-	console.log(response);
-
-	// Return a success response
-	return { success: true, message: "File sent", file: response };
 });
