690c5c99e9592b4faf1c33db4144995a535f9003
Francesco Scheffczyk <francesco.scheffczyk@gmail.com>
2024-05-27 21:39:40 +0200

    feat: Add a blinking cursor to show loading state

--- frontend/components/ScoreStream.vue
+++ frontend/components/ScoreStream.vue
@@ -2,10 +2,16 @@
 const prompt = ref<string>("What do you think of the new ruleset?");
 const score = ref<string>("");
 
+const streaming = ref<boolean>(false);
+
 async function fetchScore(prompt: string) {
 	// Clear the score
 	score.value = "";
 
+	// Show the cursor
+	streaming.value = true;
+
+	// Fetch the score
 	const response = await fetch("/api/chat/stream", {
 		method: "POST",
 		headers: {
@@ -38,6 +44,8 @@ async function fetchScore(prompt: string) {
 				try {
 					const json = JSON.parse(jsonStr.trim());
 
+					console.log("Received JSON:", json);
+
 					// Replace \\n in json.data with \n
 					if (json.data) {
 						json.data = json.data.replace(/\\n/g, "\n");
@@ -45,6 +53,10 @@ async function fetchScore(prompt: string) {
 
 					if (json.event === "data") {
 						score.value += json.data;
+					}
+
+					if (json.event === "end") {
+						streaming.value = false;
 					}
 				} catch (e) {
 					console.error("Error parsing JSON:", e);
@@ -69,9 +81,7 @@ onMounted(() => {
 <template>
 	<div class="w-1/2 max-h-[70%] flex flex-col overflow-hidden my-auto">
 		<pre
-			class="text-lg whitespace-pre-wrap overflow-auto flex-grow relative scrollbar"
-			>{{ score }}</pre
-		>
+			class="text-lg whitespace-pre-wrap overflow-auto flex-grow relative scrollbar">{{ score }}<span class="blinking-cursor ml-1" v-show="streaming"> </span></pre>
 		<UButton
 			variant="ghost"
 			class="text-lg text-black mt-7 p-0 hover:bg-transparent hover:underline"
@@ -87,5 +97,24 @@ onMounted(() => {
 .scrollbar {
 	scrollbar-color: #000000 transparent;
 	scrollbar-width: thin;
+}
+
+.blinking-cursor {
+	display: inline-block;
+	width: 0.75ch;
+	height: 2.5ch;
+	background-color: black;
+	animation: blink 1s step-end infinite;
+}
+
+@keyframes blink {
+	0%,
+	50% {
+		background-color: black;
+	}
+	50.01%,
+	100% {
+		background-color: transparent;
+	}
 }
 </style>
