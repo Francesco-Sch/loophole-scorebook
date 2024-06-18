2cc60658c30b740807ea9b4513c92ed7f091df72
Francesco Scheffczyk <francesco.scheffczyk@gmail.com>
2024-06-18 17:47:48 +0200

    docs: Minor code changes and README update

--- backend/app/utils/embeddings_function.py
+++ backend/app/utils/embeddings_function.py
@@ -3,6 +3,6 @@ import os
 from langchain_community.embeddings import OllamaEmbeddings
 
 embeddings_function = OllamaEmbeddings(
-    base_url=os.getenv("OLLAMA_EMBEDDING_URL", "http://localhost:11434"),
-    model=os.getenv("OLLAMA_EMBEDDING_MODEL", "nomic-embed-text"),
+    base_url=os.getenv("OLLAMA_URL", "http://localhost:11434"),
+    model=os.getenv("OLLAMA_MODEL", "nomic-embed-text"),
 )
